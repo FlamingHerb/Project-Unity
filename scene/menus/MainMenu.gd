c55fc534d43ac73d59f1603cc65edf3b093b1207
FlamingHerb <55610651+FlamingHerb@users.noreply.github.com>
2023-03-05 12:45:55 +0800

    :sparkles: :construction: Adding WIP Menu Scenes

---
+++ scene/menus/MainMenu.gd
@@ -0,0 +1,42 @@
+extends Control
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
+
+#===============================================================================
+# START: BUTTONS
+#===============================================================================
+
+func _on_new_game_pressed():
+	pass # Replace with function body.
+
+
+func _on_load_game_pressed():
+	pass # Replace with function body.
+
+
+func _on_settings_pressed():
+	pass # Replace with function body.
+
+
+func _on_help_pressed():
+	pass # Replace with function body.
+
+
+func _on_about_pressed():
+	pass # Replace with function body.
+
+
+func _on_exit_pressed():
+	get_tree().quit()
+
+#===============================================================================
+# END: BUTTONS
+#===============================================================================
