1bc055031b6d378d73f3400e2d71cbd2288d1340
FlamingHerb <55610651+FlamingHerb@users.noreply.github.com>
2023-03-08 22:35:19 +0800

    :sparkles: Adding basic setting functionalities

--- scene/menus/Settings.gd
+++ scene/menus/Settings.gd
@@ -13,3 +13,54 @@ func _process(delta):
 
 func _on_back_button_pressed():
 	hide()
+
+
+#==============================================================================
+# ** Master Volume
+#------------------------------------------------------------------------------
+#  
+#==============================================================================
+
+func _on_master_sound_slider_value_changed(value):
+	print(linear_to_db(value))
+	AudioServer.set_bus_volume_db(0, linear_to_db(value))
+
+
+#==============================================================================
+# ** Fullscreen
+#------------------------------------------------------------------------------
+#	TODO: Rework code
+#==============================================================================
+
+func _on_window_box_toggled(button_pressed):
+	DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_WINDOWED)
+	$DisplayGroup/ResOptions.disabled = false
+
+
+func _on_fullscreen_box_toggled(button_pressed):
+	DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_FULLSCREEN)
+	$DisplayGroup/ResOptions.disabled = true
+
+
+#==============================================================================
+# ** Resolution
+#------------------------------------------------------------------------------
+#	1 - 1280 x 720
+#	2 - 1600 x 900
+#	3 - 1920 x 1080
+#==============================================================================
+
+func _on_res_options_item_selected(index):
+	match index:
+		0:
+			DisplayServer.window_set_size(Vector2i(1280, 720))
+		1:
+			DisplayServer.window_set_size(Vector2i(1600, 900))
+		2:
+			DisplayServer.window_set_size(Vector2i(1920, 1080))
+
+func _on_vsync_button_toggled(button_pressed):
+	if button_pressed:
+		DisplayServer.window_set_vsync_mode(DisplayServer.VSYNC_ENABLED)
+	else:
+		DisplayServer.window_set_vsync_mode(DisplayServer.VSYNC_DISABLED)
