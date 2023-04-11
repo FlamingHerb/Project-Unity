df9993580419ad74d6b3b555b137766df7cbd28f
FlamingHerb <55610651+FlamingHerb@users.noreply.github.com>
2023-04-12 03:21:40 +0800

    :wheelchair: Added ability to save settings.

--- scene/levels/levelexample2.gd
+++ scene/levels/levelexample2.gd
@@ -7,11 +7,11 @@ func _ready():
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
+func _process(_delta):
 	pass
 
 
-func _on_closed_door_input_event(viewport, event, shape_idx):
+func _on_closed_door_input_event(_viewport, event, _shape_idx):
 	if event is InputEventMouseButton and event.pressed and event.button_index == MOUSE_BUTTON_LEFT:
 		if Inventory.check_item("debug_key"):
 			SceneManager.goto_scene("res://scene/levels/closedroom.tscn")
@@ -19,7 +19,7 @@ func _on_closed_door_input_event(viewport, event, shape_idx):
 			DialogueScreen.init_dialogue("door_locked")
 
 
-func _on_open_door_input_event(viewport, event, shape_idx):
+func _on_open_door_input_event(_viewport, event, _shape_idx):
 	if event is InputEventMouseButton and event.pressed and event.button_index == MOUSE_BUTTON_LEFT:
 		SceneManager.goto_scene("res://scene/levels/hallway.tscn")
 
--- scene/menus/settings.gd
+++ scene/menus/settings.gd
@@ -1,5 +1,15 @@
 extends Control
 
+@onready var master_volume = $MusicAndDisplayGroup/MasterSoundSlider
+@onready var bgm_volume = $MusicAndDisplayGroup/MusicSlider
+@onready var se_volume = $MusicAndDisplayGroup/SESlider
+@onready var amb_volume = $MusicAndDisplayGroup/AmbienceSlider
+@onready var text_speed = $MusicAndDisplayGroup/TextSpeedSlider
+@onready var auto_speed = $MusicAndDisplayGroup/AutoSpeedSlider
+@onready var window_mode = $DisplayGroup/WindowModeBoxes/WindowBox
+@onready var fullscreen_mode = $DisplayGroup/WindowModeBoxes/FullscreenBox
+@onready var resolution = $DisplayGroup/ResOptions
+@onready var vsync = $DisplayGroup/VSync
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -7,9 +17,30 @@ func _ready():
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
+func _process(_delta):
 	pass
 
+# Saves settings.
+func _save_settings():
+	# Create new ConfigFile object.
+	var config = ConfigFile.new()
+
+	# Store some values.
+	config.set_value("Music", "master_volume", master_volume.value)
+	config.set_value("Music", "bgm_volume", bgm_volume.value)
+	config.set_value("Music", "se_volume", se_volume.value)
+	config.set_value("Music", "amb_volume", amb_volume.value)
+	config.set_value("Text", "text_speed", text_speed.value)
+	config.set_value("Text", "auto_speed", auto_speed.value)
+	if window_mode.button_pressed == true:
+		config.set_value("Window", "window_mode", "fullscreen")
+	else:
+		config.set_value("Window", "window_mode", "window")
+	config.set_value("Window", "screen_res", DisplayServer.window_get_size())
+	config.set_value("Window", "vsync", vsync.button_pressed)
+
+	# Save it to a file (overwrite if already exists).
+	config.save("user://settings.cfg")
 
 #==============================================================================
 # ** Master Volume
@@ -28,12 +59,12 @@ func _on_master_sound_slider_value_changed(value):
 #	TODO: Rework code
 #==============================================================================
 
-func _on_window_box_toggled(button_pressed):
+func _on_window_box_toggled(_button_pressed):
 	DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_WINDOWED)
 	$DisplayGroup/ResOptions.disabled = false
 
 
-func _on_fullscreen_box_toggled(button_pressed):
+func _on_fullscreen_box_toggled(_button_pressed):
 	DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_FULLSCREEN)
 	$DisplayGroup/ResOptions.disabled = true
 
@@ -64,3 +95,15 @@ func _on_vsync_button_toggled(button_pressed):
 
 func _on_backbutton_pressed():
 	hide()
+
+
+func _on_restore_default_button_pressed():
+	_save_settings()
+
+
+func _on_draw():
+	print("Settings drawn.")
+
+
+func _on_hidden():
+	print("Settings hidden.")
