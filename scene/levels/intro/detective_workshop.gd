418b1a5198e572dd1632d83c4b0b2d83bd68c363
FlamingHerb <55610651+FlamingHerb@users.noreply.github.com>
2023-04-17 01:27:10 +0800

    :truck: Adding detective's bedroom

--- scene/levels/intro/detective_workshop.gd
+++ scene/levels/intro/detective_workshop.gd
@@ -9,3 +9,16 @@ func _ready():
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	pass
+
+
+func _on_open_door_input_event(_viewport:Node, event:InputEvent, _shape_idx:int):
+	if event is InputEventMouseButton and event.pressed and event.button_index == MOUSE_BUTTON_LEFT:
+		SceneManager.goto_level_scene("intro/detective_bedroom.tscn")
+	
+func _on_open_door_mouse_exited():
+	$BedroomDoor/Line2D/AnimationPlayer.stop()
+
+
+func _on_open_door_mouse_entered():
+	$BedroomDoor/Line2D/AnimationPlayer.play("glow")
+
