84f73ce5aed0ea9ee49382149f2ed52965ce3474
FlamingHerb <55610651+FlamingHerb@users.noreply.github.com>
2023-04-17 01:09:04 +0800

    :truck: Adding prologue area.

--- scene/menus/newgame.gd
+++ scene/menus/newgame.gd
@@ -15,7 +15,7 @@ func _on_back_button_pressed():
 
 
 func _on_new_game_button_pressed():
-	SceneManager.goto_scene("res://scene/levels/junction.tscn")
+	SceneManager.goto_level_scene("intro/detective_workshop.tscn")
 	GamePauseUI.toggle_ui(true)
 	DialogueScreen.toggle_ui(true)
 	#GamePauseUi.get_node("PauseCanvasLayer").show() # This has to be the most shittiest way to do stuff oml
