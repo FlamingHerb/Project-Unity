5cea93c2e578b3da4da3cd18e1039500be09845b
FlamingHerb <55610651+FlamingHerb@users.noreply.github.com>
2023-04-19 22:24:51 +0800

    :poop: Bug found. Omi is an idiot.

--- scene/menus/settings.gd
+++ scene/menus/settings.gd
@@ -13,7 +13,7 @@ extends Control
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	#_load_settings()
+	_load_settings()
 	pass
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
