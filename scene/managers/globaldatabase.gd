06aec61554538269e8a0c95876ce5a53156d84e2
FlamingHerb <55610651+FlamingHerb@users.noreply.github.com>
2023-04-28 07:03:38 +0800

    :technologist: Added switches.

--- scene/managers/globaldatabase.gd
+++ scene/managers/globaldatabase.gd
@@ -1,6 +1,10 @@
 extends Node
 
 var item_database
+var switches = {
+	"workroom_dialogue" = false,
+	"bedroom_dialogue" = false
+}
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -10,3 +14,11 @@ func _ready():
 func read_from_JSON(path):
 	var file = FileAccess.open(path, FileAccess.READ)
 	return JSON.parse_string(file.get_as_text())
+
+## Takes the switch's name and toggles it to the specified boolean value.
+func toggle_switch(switch_name, boolean_value):
+	switches[switch_name] = boolean_value
+
+## Check's the following switch's boolean value.
+func check_switch(switch_name):
+	return switches[switch_name]
