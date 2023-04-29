f8a53f73b74023644a483360ff12964b5de17810
FlamingHerb <55610651+FlamingHerb@users.noreply.github.com>
2023-04-29 17:38:11 +0800

    :bulb: Added comments to the global database code.

--- scene/managers/globaldatabase.gd
+++ scene/managers/globaldatabase.gd
@@ -1,19 +1,37 @@
 extends Node
 
+## The global database of the game.
+##
+## The following system operates on the same vein as of
+## an RPG Maker-made game. It has a switches, it remembers
+## the items that you have, and processes any other
+## information within the game.
+##
+## @tutorial:            https://the/tutorial1/url.com
+## @tutorial(Tutorial2): https://the/tutorial2/url.com
+
+##===============================================
+## Variables
+##===============================================
+
 var item_database
 var switches = {
 	"workroom_dialogue" = false,
 	"bedroom_dialogue" = false
 }
 
+##===============================================
+## Godot Processing functions
+##===============================================
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	item_database = read_from_JSON("res://assets/data/item_database.json")
 	print(item_database)
 
-func read_from_JSON(path):
-	var file = FileAccess.open(path, FileAccess.READ)
-	return JSON.parse_string(file.get_as_text())
+##===============================================
+## Switch-related functions
+##===============================================
 
 ## Takes the switch's name and toggles it to the specified boolean value.
 func toggle_switch(switch_name, boolean_value):
@@ -27,3 +45,11 @@ func check_switch(switch_name):
 func reset_switches():
 	for index in switches:
 		switches[index] = false
+
+##===============================================
+## Other related functions
+##===============================================
+
+func read_from_JSON(path):
+	var file = FileAccess.open(path, FileAccess.READ)
+	return JSON.parse_string(file.get_as_text())
