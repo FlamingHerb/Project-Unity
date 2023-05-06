675cc232382ad838f496d084b7cf04a4ca7fa6ed
FlamingHerb <55610651+FlamingHerb@users.noreply.github.com>
2023-05-06 19:49:15 +0800

    :construction: Added middle cutscene.

---
+++ scene/levels/cutscene/middle.gd
@@ -0,0 +1,18 @@
+extends Node
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	_cutscene()
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(_delta):
+	pass
+
+
+func _cutscene():
+	$AnimationPlayer.play("cutscene")
+
+func _switch():
+	SceneManager.goto_level_scene("intro/detective_bedroom.tscn")
