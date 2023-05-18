148b62be00e6284701cc681c02149643d735c0c6
FlamingHerb <55610651+FlamingHerb@users.noreply.github.com>
2023-05-19 03:59:27 +0800

    :technologist: Addition of a timer system for the final sequence.

--- scene/managers/global_timer.gd
+++ scene/managers/global_timer.gd
@@ -1,5 +1,10 @@
 extends Node
 
+signal global_timer_timeout
+
+var time_needed = 0
+@onready var the_timer = $TimeLayer/Timer
+@onready var the_timer_text = $TimeLayer/TimerText
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -10,6 +15,26 @@ func _ready():
 func _process(_delta):
 	pass
 
+func _text_time_update(seconds: int):
+	var minutes = time_needed / 60
+	var secondss = time_needed % 60
+	var str_elapsed = "%02d : %02d" % [minutes, secondss]
+	the_timer_text.text = str_elapsed
+
+## Set time by the seconds.
+func ready_time(seconds: int):
+	time_needed = seconds
+
+	_text_time_update(time_needed)
+
+	the_timer.start()
 
 func _on_timer_timeout():
-	pass # Replace with function body.
+	time_needed -= 1
+
+	_text_time_update(time_needed)
+
+	if !time_needed:
+		global_timer_timeout.emit()
+		the_timer.stop()
+
