8c3fafca934adafcf33f2872c3959db77d717c25
FlamingHerb <55610651+FlamingHerb@users.noreply.github.com>
2023-05-21 02:55:18 +0800

    :sparkle: Added splash screen.

---
+++ scene/splash_screen.gd
@@ -0,0 +1,18 @@
+extends Node
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	# if OS.is_debug_build():
+	# 	_switch()
+	# 	return
+	
+	$AnimationPlayer.play("splash")
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
+
+func _switch():
+	SceneManager.goto_scene("res://scene/menus/mainmenu.tscn")
