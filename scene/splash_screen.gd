484da02024aad48fc748d756f574525721acb06a
FlamingHerb <55610651+FlamingHerb@users.noreply.github.com>
2023-05-22 20:58:24 +0800

    :lipstick: Added skips to the splash-screen.

--- scene/splash_screen.gd
+++ scene/splash_screen.gd
@@ -14,5 +14,12 @@ func _ready():
 func _process(_delta):
 	pass
 
+func _input(_event):
+	if Input.is_action_pressed("ui_accept"):
+		if $AnimationPlayer.current_animation_position < 4.5:
+			$AnimationPlayer.seek(4.5, true)
+		if $AnimationPlayer.current_animation_position < 12 and $AnimationPlayer.current_animation_position > 4.5:
+			$AnimationPlayer.seek(12, true)
+
 func _switch():
 	SceneManager.goto_scene("res://scene/menus/mainmenu.tscn")
