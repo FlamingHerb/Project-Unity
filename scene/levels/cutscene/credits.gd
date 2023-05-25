496dc89b69676022d926d3ce5b554c742abd4309
FlamingHerb <55610651+FlamingHerb@users.noreply.github.com>
2023-05-25 21:39:37 +0800

    :running: QUICK MERGE.

--- scene/levels/cutscene/credits.gd
+++ scene/levels/cutscene/credits.gd
@@ -16,5 +16,5 @@ func _input(event):
 		SceneManager.goto_scene("res://scene/menus/mainmenu.tscn")
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
+func _process(_delta):
 	pass
