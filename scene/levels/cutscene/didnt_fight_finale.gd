ed4204b83d95813d3d4780b78f8c141560711201
FlamingHerb <55610651+FlamingHerb@users.noreply.github.com>
2023-05-25 20:29:02 +0800

    :construction: Allow player to go to didn't fight ending.

--- scene/levels/cutscene/didnt_fight_finale.gd
+++ scene/levels/cutscene/didnt_fight_finale.gd
@@ -3,9 +3,25 @@ extends Node
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	$AnimationPlayer.play("didntfight")
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
+func _process(_delta):
 	pass
+
+func _input(event):
+	if Input.is_action_pressed("ui_accept") or GlobalDatabase.is_mouse_clicked(event):
+		if $AnimationPlayer.current_animation_position < 21.9:
+			$AnimationPlayer.seek(21.9, true)
+
+func _switch_to_game():
+	#SceneManager.goto_scene("res://scene/menus/mainmenu.tscn")
+	# GamePauseUI.toggle_ui(false)
+	# DialogueScreen.toggle_ui(false)
+
+	# Inventory.reset()
+	# GlobalDatabase.reset_switches()
+	# GamePauseUI.clear_textlog()
+
+	SceneManager.goto_level_scene("cutscene/news.tscn")
